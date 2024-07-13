edabffd942ff722877fea599bac42464da4d39e0
wanchanok062 <anvacom2@gmail.com>
2024-07-13 17:37:49 +0700

    [delete] delete domain
    delete because not use it.

--- src/api/routes/index.mjs
+++ src/api/routes/index.mjs
@@ -1,11 +1,9 @@
 import express from 'express';
 
-import userRouters from './userRouters.mjs';
 /* PLOP_INJECT_IMPORT_ROUTES */
 
 const router = express.Router();
 
-router.use('/user', userRouters);
 /* PLOP_INJECT_ROUTES */
 
 export default router;
