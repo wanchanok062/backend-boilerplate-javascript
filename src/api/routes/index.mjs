0c79a599bb8900dfd436fe916a888f2bc19f0adf
wanchanok062 <anvacom2@gmail.com>
2024-07-16 21:54:30 +0700

    [edit] add new package for rate limit call api

--- src/api/routes/index.mjs
+++ src/api/routes/index.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import userRouters from './userRouters.mjs';
 /* PLOP_INJECT_IMPORT_ROUTES */
 
 const router = express.Router();
 
+router.use('/user', userRouters);
 /* PLOP_INJECT_ROUTES */
 
 export default router;
